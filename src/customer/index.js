315b0af14243d350f665c5f6aeb2beabd1269bc9
cfbender <codyfbender@gmail.com>
2019-09-26 22:19:03 -0600

    Modularize stock to be used in manager and restructure files

--- src/customer/index.js
+++ src/customer/index.js
@@ -1,18 +1,10 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const chalk = require("chalk");
-
-//function to center text in the table
-const padString = (str, endLength) => {
-  let padStr = str.toString();
-  let left = Math.floor((endLength - padStr.length) / 2) + padStr.length;
-  let right = Math.floor((endLength - padStr.length) / 2) + left;
-  return padStr.padStart(left + 1, " ").padEnd(right, " ");
-};
+const stock = require("../stock");
 
 module.exports = () => {
   //configure the connection
-  connection = mysql.createConnection({
+  const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -20,63 +12,7 @@ module.exports = () => {
     database: "bamazon"
   });
 
-  const itemIDs = [];
-
-  const stock = async () => {
-    return new Promise(async resolve => {
-      //print out header with color
-      console.log(
-        chalk.green(
-          "   _____________________________________________________________________________________"
-        )
-      );
-      console.log(
-        chalk.green(
-          "   |                                   WELCOME TO BAMAZON                              |"
-        )
-      );
-      console.log(
-        chalk.green(
-          "   -------------------------------------------------------------------------------------"
-        )
-      );
-      console.log(
-        chalk.blue(
-          "   | Item ID |                       Product                     |   Price   |  Stock  |"
-        )
-      );
-      console.log(
-        chalk.blue(
-          "   -------------------------------------------------------------------------------------"
-        )
-      );
-      const query =
-        "SELECT item_id, product_name, price, stock_quantity FROM products";
-
-      connection.query(query, (err, res) => {
-        if (err) throw err;
-        //logs each item out with correct length padding
-        res.forEach(item => {
-          itemIDs.push(item.item_id);
-          console.log(
-            `   |${padString(item.item_id, 9)}|${padString(
-              item.product_name,
-              51
-            )}|${padString(item.price, 11)}|${padString(
-              item.stock_quantity,
-              9
-            )}|`
-          );
-          console.log(
-            "   -------------------------------------------------------------------------------------"
-          );
-        });
-        resolve();
-      });
-    });
-  };
-
-  const itemSelect = async () => {
+  const itemSelect = async itemIDs => {
     //get ID of item to purchase
     let { id } = await inquirer.prompt([
       {
@@ -145,8 +81,8 @@ module.exports = () => {
 
   connection.connect(async err => {
     if (err) throw err;
-    await stock();
-    let id = await itemSelect();
+    let itemIDs = await stock(connection);
+    let id = await itemSelect(itemIDs);
     main(id);
   });
 };
---
+++ src/stock.js
@@ -0,0 +1,71 @@
+/*
+function displays all items in database formatted with id, name, price and quantity
+
+@param: connection - an active, valid MySQL connection using mysql.createConnection();
+returns: an array of all item IDs on a promise
+*/
+
+const chalk = require("chalk");
+
+//function to center text in the table
+
+const padString = (str, endLength) => {
+  let padStr = str.toString();
+  let left = Math.floor((endLength - padStr.length) / 2) + padStr.length;
+  let right = Math.floor((endLength - padStr.length) / 2) + left;
+  return padStr.padStart(left + 1, " ").padEnd(right, " ");
+};
+
+const stock = async connection => {
+  return new Promise(resolve => {
+    //print out header with color
+    console.log(
+      chalk.green(
+        "   _____________________________________________________________________________________"
+      )
+    );
+    console.log(
+      chalk.green(
+        "   |                                   WELCOME TO BAMAZON                              |"
+      )
+    );
+    console.log(
+      chalk.green(
+        "   -------------------------------------------------------------------------------------"
+      )
+    );
+    console.log(
+      chalk.blue(
+        "   | Item ID |                       Product                     |   Price   |  Stock  |"
+      )
+    );
+    console.log(
+      chalk.blue(
+        "   -------------------------------------------------------------------------------------"
+      )
+    );
+    const query =
+      "SELECT item_id, product_name, price, stock_quantity FROM products";
+
+    connection.query(query, (err, res) => {
+      let itemIDs = [];
+      if (err) throw err;
+      //logs each item out with correct length padding
+      res.forEach(item => {
+        itemIDs.push(item.item_id);
+        console.log(
+          `   |${padString(item.item_id, 9)}|${padString(
+            item.product_name,
+            51
+          )}|${padString(item.price, 11)}|${padString(item.stock_quantity, 9)}|`
+        );
+        console.log(
+          "   -------------------------------------------------------------------------------------"
+        );
+      });
+      resolve(itemIDs);
+    });
+  });
+};
+
+module.exports = stock;
